b6cc7089455704a22d949b5f4ce90e12cdc9a5cb
Piotr <36728246+pioter909@users.noreply.github.com>
2019-03-30 19:31:06 +0100

    lab2
    koniec

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -24,16 +24,40 @@ namespace GraMonolitycznie
 #if(DEBUG)
 
             Console.WriteLine(wylosowana); //usunąc w wersji release
-              
+
 
 #endif
+            //wykonuj
+            bool trafiono = false;
+            do
+            { 
+            //2 człowiek daje propozycje
+            #region
+            Console.Write("Podaj swoją propozycje: ");
+            string tekst = Console.ReadLine();
+if( tekst.ToLower(== X))
 
-            Console.Write("Podaj swoją propozycje: "); 
-            string tekst Console.ReadLine();
-            int propozycja = Convert.ToInt32(tekst);
+            int propozycja = 0;
+            try
+            {
+                propozycja = Convert.ToInt32(tekst);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Nie podano liczby");
+                    continue;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Liczba nie mieści się w rejestrze!");
+                    continue;
+            }
+
             Console.WriteLine($"Przyjąłem wartość {propozycja}");
+            #endregion
 
-
+            //3 komputer ocenia propozycje
+            #region
 
             if (propozycja < wylosowana)
                 Console.WriteLine("za mało");
@@ -41,6 +65,12 @@ namespace GraMonolitycznie
                 Console.WriteLine("za dużo");
             else
                 Console.WriteLine("trafiono");
+                trafiono = true;
+                #endregion
+
+            }
+            while( !trafiono );
+//do momentu trafienia
             Console.WriteLine("koniec gry");
         }
     }
